067d5392c40cfdb5f411073fb65cbf48edde51d5
matt <matt@weingarten.org>
2020-03-12 15:45:19 +0100

    Adds timing, scaling to millions and attempt at var99

--- deloiite.py
+++ deloiite.py
@@ -2,34 +2,38 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import norm
-import math  
+from scipy import stats
+import math
+import time
+
+start_time = time.time()
 ##SET up correlation matrix:
 ##      CH EU US
 ## CH   1 0.6 0.3
 ## EU   0.6 1 0.4
 ## US   0.3 0.4 1
-region_sigma = np.genfromtxt("./data/Correlation.csv",delimiter=',',skip_header=1)[: , 1:4]
+region_sigma = np.genfromtxt("./Data/Correlation.csv",delimiter=',',skip_header=1)[: , 1:4]
 region_mu = [0,0,0]
 
 ##SET up factors matrix:
 #aplha_ch, alpha_eu, alpha_us
 
-alphas = np.genfromtxt("./data/Factor_loadings.csv",delimiter=',', skip_header=1)[:,1:4]
+alphas = np.genfromtxt("./Data/Factor_Loadings.csv",delimiter=',', skip_header=1)[:,1:4]
 
 
 ##SET mapping loan type to rating
-ratvalues = np.genfromtxt("./data/PD_Table.csv",delimiter=',',skip_header=1)[:,1:2]
+ratvalues = np.genfromtxt("./Data/PD_Table.csv",delimiter=',',skip_header=1)[:,1:2]
 for i in range(ratvalues.size):
     ratvalues[i] = norm.ppf(ratvalues[i])
 
 def rat2index(str):
     if (str == b'AAA'):
         return 0
-    elif(str==b'AA'): 
+    elif(str==b'AA'):
         return 1
     elif(str==b'A'):
         return 2
-    elif(str==b'BBB'): 
+    elif(str==b'BBB'):
         return 3
     elif(str==b'BB'):
         return 4
@@ -52,7 +56,7 @@ id2int = lambda x : int(x[3 :])
 ## Region converters : CH to 0, EU to 1, US to 2
 reg2int = lambda x : 0 if (x == b'CH') else (1 if (x == b'EU') else 2)
 
-##Rating converters : indexes into values from PD_table csv according to strings: AAA, AA, A, BBB, BB, B, CCC, or default, 
+##Rating converters : indexes into values from PD_table csv according to strings: AAA, AA, A, BBB, BB, B, CCC, or default,
 
 rat2float = lambda x : ratvalues[rat2index(x)]
 
@@ -63,7 +67,7 @@ rat2float = lambda x : ratvalues[rat2index(x)]
 
 ##Loan, Region,Rating(proabability of default),EAD(loan amount), LGD(Loss given default)
 
-portfolio = np.genfromtxt("./data/Portfolio.csv", delimiter=',',skip_header=1,converters={0: id2int,1: reg2int, 2: rat2float},dtype=[('id', int),('region',int),('thresh',float),('amount',float),('ldg',float)])
+portfolio = np.genfromtxt("./Data/Portfolio.csv", delimiter=',',skip_header=1,converters={0: id2int,1: reg2int, 2: rat2float},dtype=[('id', int),('region',int),('thresh',float),('amount',float),('ldg',float)])
 ## size checks seem to be correct ( size = 38401, consistent with csv file)
 assert(portfolio.size == alphas.size/3), "size mismatch"
 
@@ -83,20 +87,25 @@ def simulation():
         value = np.dot(regsample, alphas[i]) + gammas[i] * loansample[i]
         if(value <= portfolio[i]['thresh']): res += portfolio[i]['ldg'] * portfolio[i]['amount']
     return res
- 
+
 
 ##runs simulation 100 000 times (super slow for 100 000)
 def montecarlo():
     n = 100
     sims = np.zeros(n)
     for i in range(n):
-        sims[i] = simulation()
+        sims[i] = int(simulation()/1000000)
+        # print("This simulation we lost: ")
+        # print(sims[i])
+        # print("Million")
+        # print("____________________________")
+        #   time.sleep(0.5)
     return sims
 
 #displays results ins histogram(the histogramm sucks)
 def displayhisto(sims, mean, std):
     n = sims.size
-    max = mean +   std
+    max = mean +  std
     min = mean -  std
     histo, bins = np.histogram(sims,20,(min,max))
     plt.hist(sims,bins)
@@ -104,18 +113,26 @@ def displayhisto(sims, mean, std):
 
 ##returns mean and standard deviation  estimation from data
 def describemonte(sims):
-    
+
     n = sims.size
     std = 0
     mean = np.average(sims)
     for i in range(n):
         std += math.pow((sims[i] - mean), 2)
-    return (mean, std/n)   
+    return (mean, math.sqrt(std/n))
 
 
 sims = montecarlo()
 mu, std = describemonte(sims)
-#displayhisto(sims, mu, std)
+
+var95 = np.percentile(sims,0.95)
+var99 = np.percentile(sims,0.99)
+print("var95: %s" % var95)
+print("var99: %s" % var99)
 
 print((mu,std))
-print(int(np.sum(sims)/ 1000000))
+
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+displayhisto(sims, mu, std)
